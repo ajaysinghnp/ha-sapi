b816373b687d516013d4fba2397e1b335d8f88c4
Ajay Singh <admin@ajaysingh.com.np>
2025-01-21 19:27:08 +0545

    fix version bumped for common js file importing and registering all the cards

---
+++ custom_components/sapi/frontend/sapi.js
@@ -0,0 +1,5 @@
+import TestCard from "./test-card";
+import PanDetailsCard from "./pan-details-card";
+
+customElements.define("test-card", TestCard);
+customElements.define("pan-details-card", PanDetailsCard);
